552e036c5aaebeaba784d63da1812b7d10f06a44
Kaito <kaaitokid@gmail.com>
2016-10-08 00:18:44 +0200

    mucs editor

---
+++ src/Editor.cpp
@@ -0,0 +1,55 @@
+#include "Editor.hpp"
+
+Editor::Editor(sf::RenderWindow* pwindow){
+    window = pwindow;
+    mousePressed = false;
+}
+    
+void Editor::run(){
+    
+    while(window->isOpen()){
+
+        sf::Event event;
+        while(window->pollEvent(event)){
+            switch (event.type){
+                case sf::Event::Closed:
+                    window->close();
+                    exit(0);
+                case  sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Escape) {
+                        return;
+                    }
+                    if (event.key.code == sf::Keyboard::S) {
+                        //save
+                    }
+                default:
+                    break;
+            }
+        }   
+
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+            if(!mousePressed){
+                mousePressed = true;
+                float mouse_x, mouse_y;
+                mouse_x = sf::Mouse::getPosition(*window).x; 
+                mouse_y = sf::Mouse::getPosition(*window).y;
+                
+                sf::CircleShape auxCircle (50, 50);
+                auxCircle.setPosition(mouse_x, mouse_y);
+                circles.push_back(auxCircle);
+            }
+        }
+        else { 
+            mousePressed = false;
+        }
+        
+        window->clear();
+
+        for(size_t i = 0; i < circles.size(); ++i){
+            window->draw(circles[i]);
+        }
+        
+        window->display();
+    }
+    
+}
