1b4c1e8eedfc39928cb38ab839d8e87ff3b725f2
Kaito <kaaitokid@gmail.com>
2016-10-08 02:13:04 +0200

    Editor update

--- src/Editor.cpp
+++ src/Editor.cpp
@@ -1,4 +1,5 @@
 #include "Editor.hpp"
+#include <cmath>
 
 Editor::Editor(sf::RenderWindow* pwindow){
     window = pwindow;
@@ -19,29 +20,60 @@ void Editor::run(){
                     if (event.key.code == sf::Keyboard::Escape) {
                         return;
                     }
+                    if (event.key.code == sf::Keyboard::U) {
+                        if(!mousePressed){
+                            if(circles.size() > 0){
+                                circles.pop_back();
+                                littleCircle.pop_back();
+                            }
+                        }
+                    }
                     if (event.key.code == sf::Keyboard::S) {
-                        //save
+                        std::ofstream aux("filename.txt");
+                        if(aux.is_open()){
+                            for(size_t i = 0; i < circles.size(); ++i){
+                                
+                                auto c = circles[i];
+                                auto lc = littleCircle[i];
+                                aux << c.getPosition().x << " " << c.getPosition().y << " " << lc.getPosition().x - c.getPosition().x << " " << lc.getPosition().y - c.getPosition().y << '\n';
+                                littleCircle[i];
+                            }
+                            aux.close();
+                        }
                     }
                 default:
                     break;
             }
         }   
 
+        float mouse_x, mouse_y;
+        mouse_x = sf::Mouse::getPosition(*window).x; 
+        mouse_y = sf::Mouse::getPosition(*window).y;
         if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-            if(!mousePressed){
+            if(!mousePressed){ //82 de mida 
                 mousePressed = true;
-                float mouse_x, mouse_y;
-                mouse_x = sf::Mouse::getPosition(*window).x; 
-                mouse_y = sf::Mouse::getPosition(*window).y;
-                
-                sf::CircleShape auxCircle (50, 50);
-                auxCircle.setOrigin(25,25);
+                sf::CircleShape auxCircle(40, 79);
+                auxCircle.setOrigin(40,40);
                 auxCircle.setFillColor(sf::Color(0,0,0));
                 auxCircle.setPosition(mouse_x, mouse_y);
                 circles.push_back(auxCircle);
             }
         }
         else { 
+            if(mousePressed){
+                sf::CircleShape auxCircle(2, 20);
+                auxCircle.setOrigin(2,2);
+                auxCircle.setFillColor(sf::Color(255,0,0));
+                
+                auto p1 = sf::Vector2f(circles[circles.size()-1].getPosition());
+                auto p2 = sf::Vector2f(mouse_x, mouse_y);
+                auto vector = sf::Vector2f(p2.x-p1.x, p2.y-p1.y);
+                float module = std::sqrt(vector.x*vector.x + vector.y*vector.y);
+                sf::Vector2f unitVector(vector.x/module, vector.y/module);
+                sf::Vector2f finalPoint(unitVector.x*82, unitVector.y*82);
+                auxCircle.setPosition(p1.x+finalPoint.x, p1.y+finalPoint.y);
+                littleCircle.push_back(auxCircle);
+            }
             mousePressed = false;
         }
         
@@ -50,7 +82,17 @@ void Editor::run(){
         for(size_t i = 0; i < circles.size(); ++i){
             window->draw(circles[i]);
         }
-        
+        for(size_t i = 0; i < littleCircle.size(); ++i){
+            window->draw(littleCircle[i]);
+            
+            sf::Vertex line[] =
+                {                    
+                      sf::Vertex(circles[i].getPosition()),
+                      sf::Vertex(littleCircle[i].getPosition())
+                };
+
+            window->draw(line, 2, sf::Lines);
+        }
         window->display();
     }
     
