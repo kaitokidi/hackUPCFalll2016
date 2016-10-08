a50c6cbbd9f68eabada57267183e89da24beecc6
Pinkii- <pinkiibiza@gmail.com>
2016-10-08 04:23:49 +0200

    funca

--- src/Gaem.hpp
+++ src/Gaem.hpp
@@ -28,7 +28,7 @@ struct Pajaritos {
   int vy[MAX_PAJA];
   Pajarito p[MAX_PAJA];
   bool active[MAX_PAJA];
-  int timerms[MAX_PAJA];
+  float timerms[MAX_PAJA];
   int raiorID[MAX_PAJA];
 };
 
@@ -46,7 +46,7 @@ void GaemData_ResetIDPajarito() {
 struct Raios {
   int x[MAX_RAIO];
   int y[MAX_RAIO];
-  int timerms[MAX_RAIO];
+  float timerms[MAX_RAIO];
   bool done[MAX_RAIO];
   int pajaritoID[MAX_RAIO];
 };
@@ -114,39 +114,81 @@ void loadLevel(GaemData* gd, std::string path) {
       }
     }
   }
-  for (unsigned int i = 0; i < vs.size(); ++i) {
+  for (unsigned int i = 0; i < vs.size(); i += 5) {
     int nextPajarito = GaemData__GetNewIDPajarito();
-    gd->pajaritos.x[nextPajarito]  = my_stoi(vs[i++].second);
-    gd->pajaritos.y[nextPajarito]  = my_stoi(vs[i++].second);
-    gd->pajaritos.vx[nextPajarito] = my_stoi(vs[i++].second);
-    gd->pajaritos.vy[nextPajarito] = my_stoi(vs[i++].second);
-    gd->pajaritos.p[nextPajarito] = Pajarito(my_stoi(vs[i++].second));
+    gd->pajaritos.x[nextPajarito]  = my_stoi(vs[i].second);
+    gd->pajaritos.y[nextPajarito]  = my_stoi(vs[i+1].second);
+    gd->pajaritos.vx[nextPajarito] = my_stoi(vs[i+2].second);
+    gd->pajaritos.vy[nextPajarito] = my_stoi(vs[i+3].second);
+    gd->pajaritos.p[nextPajarito] = Pajarito(my_stoi(vs[i+4].second));
+    std::cout << "Pajarituuu" << std::endl;
   }
 }
 
-void UpdateRaio(GaemData* gd, int id, int dt_milis) {
+void UpdateRaio(GaemData* gd, int id, float dt) {
   Pajaritos* p = &gd->pajaritos;
   Raios* r = &gd->raios;
   if (r->done[id]) return;
-  r->timerms[id] += dt_milis;
+  r->timerms[id] += dt;
   sf::Vector2i ini(r->x[id], r->y[id]);
-  sf::Vector2i dest = ini * dt_milis * RAIO_SPEED;
-  dest.x /= 1000;
-  dest.y /= 1000;
-  bool onGoal = false;
+  sf::Vector2i v(p->vx[r->pajaritoID[id]], p->vy[r->pajaritoID[id]]);
+  float modul =  std::sqrt(v.x * v.x + v.y * v.y);
+  sf::Vector2f vu = sf::Vector2f (v.x / modul, v.y / modul);
+  sf::Vector2i dest = ini + sf::Vector2i(vu * r->timerms[id] * float(RAIO_SPEED));
+  // std::cout << "Soy el rayo " << id << " y estoy en " << dest.x << " " << dest.y << " " << r->timerms[id] << " " << dt << std::endl;
   // Mirar si golpea con un pajarito
-  for (unsigned int i = 0; i <= ID__Raio || onGoal; ++i) {
+  for (unsigned int i = 0; i <= ID__Pajarito; ++i) {
     if (i == r->pajaritoID[id]) continue;
-    
+    sf::Vector2i pajaritoPos(p->x[i], p->y[i]);
+    sf::Vector2i v = dest - pajaritoPos;
+    float dist = std::sqrt(v.x * v.x + v.y * v.y);
+    if (dist < PAJARITO_RADIO) {
+      r->done[id] = true;
+      p->active[i] = true;
+
+      int idNewRaio = GaemData__GetNewIDRaio();
+
+      r->x[idNewRaio] = p->x[i];
+      r->y[idNewRaio] = p->y[i];
+      r->pajaritoID[idNewRaio] = i;
+      r->timerms[idNewRaio] = 0;
+      r->done[idNewRaio] = false;
+    }
   }
   // Mirar si golepa con un raio
 
 }
 
-void GaemLogic_updateGame(GaemData* gd, int dt_milis) {
-  for (unsigned int i = 0; i < MAX_RAIO; ++i) {
-      UpdateRaio(gd, i, dt_milis);
+void GaemLogic_updateGame(GaemData* gd, float dt_milis, sf::RenderWindow* target) {
+  sf::Vector2i mousePosition = sf::Mouse::getPosition(*target);
+  bool mousePressed = sf::Mouse::isButtonPressed(sf::Mouse::Button::Left);
+  if (mousePressed) {
+    for (int i = 0; i <= int(ID__Pajarito); ++i) {
+      if (gd->pajaritos.active[i]) continue;
+      int diffx = mousePosition.x - gd->pajaritos.x[i];
+      int diffy = mousePosition.y - gd->pajaritos.y[i];
+      if (std::sqrt(diffx * diffx + diffy * diffy) < PAJARITO_RADIO) {
+        std::cout << "Clicqued" << std::endl;
+        gd->pajaritos.active[i] = true;
+
+        int idNewRaio = GaemData__GetNewIDRaio();
+
+        gd->raios.x[idNewRaio] = gd->pajaritos.x[i];
+        gd->raios.y[idNewRaio] = gd->pajaritos.y[i];
+        gd->raios.pajaritoID[idNewRaio] = i;
+        gd->raios.timerms[idNewRaio] = 0;
+        gd->raios.done[idNewRaio] = false;
+      }
+    }
   }
+
+  int totalDone = 0;
+  for (int i = 0; i <= int(ID__Raio); ++i) {
+      UpdateRaio(gd, i, dt_milis);
+      totalDone += gd->raios.done[i];
+  }
+  if (totalDone == ID__Pajarito) std::cout << "WIN" << std::endl;
+
 }
 
 
