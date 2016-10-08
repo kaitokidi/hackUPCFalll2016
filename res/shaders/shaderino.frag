7acf985da0195cd8951aba7fc277440e3829664c
pnkthrepwood <pnkthrepwood@gmail.com>
2016-10-08 18:46:49 +0200

    glow i tal

---
+++ res/shaders/shaderino.frag
@@ -0,0 +1,38 @@
+uniform sampler2D texture;
+uniform vec2 pos;
+uniform float time;
+uniform bool istouch;
+
+float nsin(float x)
+{
+    return (sin(x)+1.0f)/2.0f;
+}
+
+void main() 
+{
+        // lookup the pixel in the texture
+        vec4 pixel = texture2D(texture, gl_TexCoord[0].xy);
+        vec4 color = vec4(0.0,0.0,0.0,1.0);//gl_Color;
+
+        float d = distance(gl_TexCoord[0].st, vec2(0.5f,0.5f));
+        if (d < 0.4f)
+        {
+            float cc = 0.5f + (0.5f) * nsin(time*8.0f);
+            if (isTouch)
+            {
+            color[0] = 1.0f;
+            color[1] = 0.5f;
+            color[2] = 0.5f;
+            }
+            else
+            {
+                color[0] = 0.5f;
+                color[1] = 0.5f;
+                color[2] = 1.0f;
+            }
+            color[3] = cc;
+        }
+
+       // multiply it by the color
+       gl_FragColor = color * pixel;
+}
--- src/GaemRenderer.hpp
+++ src/GaemRenderer.hpp
@@ -10,32 +10,63 @@ sf::Shader glowerino;
 sf::RenderTexture raios;
 sf::Texture pajaritoTextrure;
 
-void GaemRenderer__Render(GaemData* data, sf::RenderWindow* window) {
+
+void GaemRenderer__Render(GaemData* data, sf::RenderWindow* window) 
+{
+static sf::Shader* txader = NULL;
+    if (txader == NULL)
+    {
+        txader = new sf::Shader();
+        txader->loadFromFile("../res/shaders/shaderino.frag", sf::Shader::Type::Fragment);
+    }
+    static sf::Clock clk;
+    txader->setParameter("time", clk.getElapsedTime().asSeconds());
+
   sf::Sprite pajarito;
   pajarito.setTexture(pajaritoTextrure, true);
   pajarito.setOrigin(sf::Vector2f(pajaritoTextrure.getSize()/2u));
   // Legasida
-  for (int i = 0; i <= ID__Pajarito; ++i) {
+  
+  for (int i = 0; i <= ID__Pajarito; ++i) 
+  {
     // sf::CircleShape shape;
     sf::Vector2f circlePos(data->pajaritos.x[i], data->pajaritos.y[i]);
     pajarito.setPosition(circlePos);
     sf::Color color = (data->pajaritos.toqueteable[i] ? sf::Color::Blue : sf::Color::Red);
     pajarito.setColor(color);
-    for (int j = 0; j <= data->pajaritos.p[i]; ++j) {
+    for (int j = 0; j <= data->pajaritos.p[i]; ++j) 
+    {
       float deltaX = data->pajaritos.vx[i][j];
       float deltaY = data->pajaritos.vy[i][j];
       float angle = atan2(deltaY, deltaX) * 180 / M_PI;
       pajarito.setRotation(angle + 90);
-      window->draw(pajarito, &blurrerino);
+        txader->setParameter("pos", circlePos);
+        txader->setParameter("istouch", data->pajaritos.toqueteable[i]);
+      window->draw(pajarito, txader);
     }
   }
 
+    sf::Vector2f pos(
+            float(data->pajaritos.x[0]) / window->getSize().x, 
+            float(data->pajaritos.y[0]) / window->getSize().y 
+            );
+//    for (int i = 0; i < ID__Pajarito; ++i)
+    {
+   //     txader->setParameter("pos", pos);
+    }
+/*
+    sf::Sprite spr;
+  window->draw(spr, txader);
+*/
+  
   raios.clear(sf::Color::Transparent);
 
-  for (int i = 0; i <= ID__Raio; ++i) {
+  for (int i = 0; i <= ID__Raio; ++i) 
+  {
     sf::Vector2i ini(data->raios.x[i], data->raios.y[i]);
     sf::Vector2i dest = ini + sf::Vector2i(getIncremento(data, i, data->raios.timerms[i]));
-    sf::Vertex line[] = {
+    sf::Vertex line[] = 
+    {
       sf::Vertex(sf::Vector2f(ini)),
       sf::Vertex(sf::Vector2f(dest))
     };
@@ -44,12 +75,15 @@ void GaemRenderer__Render(GaemData* data, sf::RenderWindow* window) {
 
     raios.draw(line, 2, sf::Lines);
   }
+  ///
   raios.display();
 
   sf::Sprite raiosConjuntos;
   raiosConjuntos.setTexture(raios.getTexture());
 
-  window->draw(raiosConjuntos,&glowerino);
+  window->draw(raiosConjuntos, txader);
+
+  
   // new Shit
 
 }
